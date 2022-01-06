3f886f68a9b7f83a83d796a456856ce3728ec463
yeonx <71878202+yeonx@users.noreply.github.com>
2022-01-07 02:29:23 +0900

    Create p217-p219.py

---
+++ chapter08/p217-p219.py
@@ -0,0 +1,16 @@
+x=int(input())
+
+d=[0]*(x+1)
+
+#다이나믹 프로그래밍 진행(보텀업)
+for i in range(2,x+1):
+    d[i]=d[i-1]+1
+    if i%2==0:
+        d[i]=min(d[i],d[i//2]+1)
+    elif i%3==0:
+        d[i]=min(d[i],d[i//3]+1)
+    elif i%5==0:
+        d[i]=min(d[i],d[i//5]+1)
+        
+
+print(d[x])
