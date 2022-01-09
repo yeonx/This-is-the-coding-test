b5adb78ce0369f07251331485b3395754b0cdcdd
yeonx <71878202+yeonx@users.noreply.github.com>
2022-01-09 23:04:08 +0900

    Create p321.py
    https://www.acmicpc.net/problem/18406

---
+++ chapter11/p321.py
@@ -0,0 +1,25 @@
+N=int(input())
+
+le=0
+ri=0
+
+n=N
+count=0
+while n!=0:
+    count+=1
+    n=n//10
+
+count=count//2
+
+for i in range(count):
+    ri+=N%10
+    N=N//10
+for i in range(count):
+    le+=N%10
+    N=N//10
+if le==ri:
+    print("LUCKY")
+else:
+    print("READY")
+
+#https://www.acmicpc.net/problem/18406
