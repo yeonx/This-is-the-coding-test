1fee31ad01f63045a4b39c89784e9784a061eab7
yeonx <71878202+yeonx@users.noreply.github.com>
2022-01-13 23:16:03 +0900

    Create p314.py
    다시 풀기

---
+++ chapter11/p314.py
@@ -0,0 +1,16 @@
+#간단한 코드지만 몰랐다.. 알아두기 
+N=int(input())
+
+data=list(map(int,input().split()))
+
+data.sort()
+
+target=1
+for x in data:
+    #만들 수 없는 금액을 찾았을 때 반복 종료
+    if target <x:
+        break
+    target+=x
+    
+    #만들 수 없는 금액 출력
+    print(target)
